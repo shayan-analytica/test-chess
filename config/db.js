fd71fada9efbb7c4888e83573f162f470702e6b7
shayan-analytica <s.shayan@analytica-data.com>
2024-08-15 08:54:16 +0400

    updated db

--- config/db.js
+++ config/db.js
@@ -3,7 +3,10 @@ require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_DB_URL);
+        await mongoose.connect(process.env.MONGO_DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
         console.log('MongoDB connected');
     } catch (err) {
         console.error(err);
